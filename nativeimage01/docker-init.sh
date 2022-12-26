49784e0ec1b0ee5f2dfa971c7ceb670ff638bbdb
lukelee <lukelee01@gmail.com>
2022-12-25 21:32:50 -0500

    update Dockerfile with sdkman. create docker-compose.yml

--- nativeimage01/docker-init.sh
+++ nativeimage01/docker-init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-microdnf update -y
-microdnf install wget -y
-mkdir /usr/local/maven
-wget ${MAVEN_DOWNLOAD_URL}
-tar xzf ${MAVEN_FILE} -C /usr/local/maven/ --strip-components=1
-echo export 'PATH=$PATH:/usr/local/maven/bin/' > /etc/profile.d/maven.sh
-chmod +x /etc/profile.d/maven.sh
-source /etc/profile.d/maven.sh
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y zip unzip curl build-essential libz-dev zlib1g-dev
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 22.3.r17-grl
+sdk install maven 3.8.6
+
+gu install native-image
 mvn -Pnative clean compile package native:compile-no-fork
